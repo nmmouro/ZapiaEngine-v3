c1fdc46f3165b634b801d4b15eddeea8d270d4ca
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-09-03 14:15:44 -0300

    Update lancamentos.js

--- js/schemas/lancamentos.js
+++ js/schemas/lancamentos.js
@@ -59,7 +59,7 @@ export const SCHEMA_LANCAMENTOS = {
         // ========================================================
 
         {
-            name: "data",
+            name: "data_cadastro",
             label: "Data",
             type: "date",
             required: true,
